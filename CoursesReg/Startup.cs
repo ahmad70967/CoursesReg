85df7c98b84c9baf86b1f9576b91c726e7b9576d
admin <fatima.anm@hotmail.com>
2019-04-14 21:42:53 +0200

    Add project files.

---
+++ CoursesReg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoursesReg.Startup))]
+namespace CoursesReg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
